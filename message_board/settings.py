8222227a0f9912d5a1fd821332dd509b49c33c88
AfredTX <53712537+AfredTX@users.noreply.github.com>
2023-01-23 11:43:34 -0500

    Update settings.py

--- message_board/settings.py
+++ message_board/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!ifw=tl1r0h#atmp$$gu(3o141$&#d4g3rzt2of%suf8m-w%z'
+SECRET_KEY = <AUTH KEY>
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
